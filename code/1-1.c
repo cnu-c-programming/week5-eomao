383924adf556fd24f60e72b24a653ce4b128adb0
eomao <156084917+eomao@users.noreply.github.com>
2026-04-03 11:27:57 +0900

    Update 1-1.c

--- code/1-1.c
+++ code/1-1.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 
-void double_elements(int* arr){
-    for(int i = 0; i < 5; i++){
-        arr[i] = arr[i] * 2;
+void double_elements(int *arr, int size) {
+    int *p = arr;
+    int *end = arr + size;
+
+    while (p < end) {
+        *p = *p * 2;
+        p++;
     }
-    return;
 }
 
-int main(){
+int main() {
     int arr[5];
+
     scanf("%d %d %d %d %d", &arr[0], &arr[1], &arr[2], &arr[3], &arr[4]);
+
     double_elements(arr, 5);
-    for(int i = 0; i < 5; i++){
+
+    for (int i = 0; i < 5; i++) {
         printf("%d\n", arr[i]);
     }
+
     return 0;
 }
